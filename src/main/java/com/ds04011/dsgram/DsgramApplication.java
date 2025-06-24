92d762244957ae7af77243022f1e66f265f2c225
ds04011 <tommyhw97@gmail.com>
2025-06-24 22:00:53 +0900

    [ds04011] -기본 환경설정, mysql, mybatis, thymeleaf

---
+++ src/main/java/com/ds04011/dsgram/DsgramApplication.java
@@ -0,0 +1,13 @@
+package com.ds04011.dsgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ds04011/dsgram/DsgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds04011.dsgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
