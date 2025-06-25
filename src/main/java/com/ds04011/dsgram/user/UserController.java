55e8a978bb6ae736a23d7f14b2c1a100b1a4fbc5
ds04011 <tommyhw97@gmail.com>
2025-06-25 17:04:27 +0900

    [ds04011] - register view 구성중

---
+++ src/main/java/com/ds04011/dsgram/user/UserController.java
@@ -0,0 +1,21 @@
+package com.ds04011.dsgram.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user/view")
+public class UserController {
+	
+	@GetMapping("/login")
+	public String loginInput() {
+		return "user/login.html";
+	}
+	
+	@GetMapping("/register")
+	public String registerInput() {
+		return "user/register.html";
+	}
+
+}
